a34b96dc483e978f76c3a8464133a849d5a29a80
Wolfgang Knauer <antiker2003@gmail.com>
2024-10-09 13:23:03 +0200

    #1 Interfaces wurden erstellt

---
+++ src/main/java/interfaces/ICustomer.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+import enums.Gender;
+
+import java.time.LocalDate;
+
+public interface ICustomer {
+    void setFirstName(String firstName);
+    void setLastName(String lastname);
+    void setBirthDate(LocalDate birthDate);
+    void setGender(Gender gender);
+    String getFirstName();
+    String getLastName();
+    LocalDate getBirthDate();
+    Gender getGender();
+}
