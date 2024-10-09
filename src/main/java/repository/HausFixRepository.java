ce1f986f92387244a7d9d297d321e8dd4f378c96
Deniz Heinzelmann <deniz.heinzelmann@gmx.de>
2024-10-09 08:26:28 +0200

    Added MARIADB Database Connection and SETUP Connection with Test

---
+++ src/main/java/repository/HausFixRepository.java
@@ -0,0 +1,23 @@
+package repository;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class HausFixRepository {
+        private static final String URL = "jdbc:mariadb://localhost:3306/hausfix_db";
+        private static final String USER = "root"; // Benutzername
+        private static final String PASSWORD = "hausverwaltung"; // Passwort
+
+        public static Connection getConnection() {
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Verbindung zur Datenbank erfolgreich!");
+            } catch (SQLException e) {
+                System.out.println("Fehler bei der Verbindung zur Datenbank: " + e.getMessage());
+            }
+            return connection;
+        }
+    }
---
+++ src/main/java/repository/HausFixRepositoryTest.java
@@ -0,0 +1,10 @@
+package repository;
+
+public class HausFixRepositoryTest {
+    public class Main {
+        public static void main(String[] args) {
+            HausFixRepository.getConnection();
+        }
+    }
+
+}
