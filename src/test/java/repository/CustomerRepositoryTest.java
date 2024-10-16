8f85d63c22bfde6799b883a290446f940b27135d
Wolfgang Knauer <antiker2003@gmail.com>
2024-10-16 08:39:28 +0200

    Introduced random uuid for every test in CustomerRepositoryTest and moved DatabaseConnectionTest

--- src/test/java/repository/CustomerRepositoryTest.java
+++ src/test/java/repository/CustomerRepositoryTest.java
@@ -1,10 +1,9 @@
-package test.java.repository;
+package repository;
 
 import enums.Gender;
 import model.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import repository.CustomerRepository;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -14,6 +13,7 @@ import java.util.UUID;
 class CustomerRepositoryTest {
 
     private CustomerRepository customerRepository;
+    private UUID customerID = UUID.randomUUID();
 
     @BeforeEach
     void setUp() throws SQLException {
@@ -27,23 +27,23 @@ class CustomerRepositoryTest {
 
     @Test
     void createCustomer() {
-        Customer customer = new Customer(UUID.fromString("123e4567-e89b-42d3-a456-556642440000"),"Steve", "Müller", LocalDate.now(), Gender.M);
+        Customer customer = new Customer(this.customerID,"Steve", "Müller", LocalDate.now(), Gender.M);
         this.customerRepository.createCustomer(customer);
     }
 
     @Test
     void getCustomer() {
-        this.customerRepository.getCustomer(UUID.fromString("123e4567-e89b-42d3-a456-556642440000"));
+        this.customerRepository.getCustomer(this.customerID);
     }
 
     @Test
     void updateCustomer() {
         Customer customer = new Customer("Gary", "Müller", LocalDate.now(), Gender.W);
-        this.customerRepository.updateCustomer(UUID.fromString("123e4567-e89b-42d3-a456-556642440000"),customer);
+        this.customerRepository.updateCustomer(this.customerID,customer);
     }
 
     @Test
     void deleteCustomer() {
-        this.customerRepository.deleteCustomer(UUID.fromString("123e4567-e89b-42d3-a456-556642440000"));
+        this.customerRepository.deleteCustomer(this.customerID);
     }
 }
