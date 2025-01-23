c6c15ec0587be81dd07271f7c2ba261796b9e90b
Wolfgang Knauer <antiker2003@gmail.com>
2025-01-23 11:54:01 +0100

    #19 - Added dummy functions that yet lack logic for the needed endpoints

--- src/main/java/api/CustomerResource.java
+++ src/main/java/api/CustomerResource.java
@@ -9,19 +9,63 @@ import utils.SchemaValidator;
 
 
 import javax.xml.validation.Schema;
+import java.util.UUID;
 
-@Path("/customer")
+@Path("/customers")
 public class CustomerResource {
-    @POST
+    @GET
+    @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createCustomer(){
+    public Response getCustomerWithUUID(@PathParam("id") UUID uuid){
         //validate Request
         //execute necessary logic
         //return Response
         JSONObject schemaJson = SchemaValidator.loadSchema("");
 
 
+        return Response.status(Response.Status.OK).entity("dummy text").build();
+    }
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustomer(){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.OK).entity("dummy text").build();
+    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postCustomer(){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
         return Response.status(Response.Status.CREATED).entity("dummy text").build();
+    }
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response putCustomer(){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.CREATED).entity("dummy text").build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCustomerWithUUID(@PathParam("id") UUID uuid){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.OK).entity("dummy text").build();
     }
 }
--- src/main/java/api/ReadingResource.java
+++ src/main/java/api/ReadingResource.java
@@ -1,17 +1,70 @@
 package api;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Produces;
+import enums.KindOfMeter;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.json.JSONObject;
+import utils.SchemaValidator;
 
+import java.util.UUID;
+
+@Path("/readings")
 public class ReadingResource {
-    @PUT
+    @GET
+    @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createCustomer(){
+    public Response getReadingWithUUID(@PathParam("id") UUID uuid){
+        //validate Request
+        //execute necessary logic
+        //return Response
+        JSONObject schemaJson = SchemaValidator.loadSchema("");
 
-        return null;
+
+        return Response.status(Response.Status.OK).entity("dummy text").build();
+    }
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReading(@QueryParam("customer") UUID id, @QueryParam("start") String start, @QueryParam("end") String end, @QueryParam("kindOfMeter")KindOfMeter kindOfMeter){
+        //validate Request
+        //execute necessary logic, including the query parameters and their possible absence
+        //return Response
+
+        return Response.status(Response.Status.OK).entity("dummy text").build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postReading(){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.CREATED).entity("dummy text").build();
+    }
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response putReading(){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.CREATED).entity("dummy text").build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteReadingWithUUID(@PathParam("id") UUID uuid){
+        //validate Request
+        //execute necessary logic
+        //return Response
+
+        return Response.status(Response.Status.OK).entity("dummy text").build();
     }
 }
